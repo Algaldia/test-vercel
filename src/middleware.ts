b85604eb19230ec552656ad59e059ee96d3e71a6
diegojsa <diegojsaraujo@gmail.com>
2024-05-14 14:03:40 -0300

    adding local storage

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request : NextRequest) {
-    const country = request.geo?.country;
-    return NextResponse.json({
-        country
-    })
+    if (!(localStorage.getItem("country"))) {
+        const country = request.geo?.country;
+        if (country) {
+            localStorage.setItem("country", country);
+        }
+    }
+
+    return NextResponse.next();
     //return NextResponse.redirect(new URL("/", request.url));
 }
 

7f54edfd2f862448cd6f1fa6cfc9318a9e2b26c3
diegojsa <diegojsaraujo@gmail.com>
2024-05-15 12:06:47 -0300

    erro vercel

--- src/app/localization/page.tsx
+++ src/app/localization/page.tsx
@@ -1,26 +1,26 @@
 const devPath = 'http://localhost:3000/localization/api';
 const vercelPath = 'https://test-vercel-eight-indol.vercel.app/localization/api'
 
-async function getData() {
+/* async function getData() {
     "use server"
     const response = await fetch(vercelPath, {
         method: "GET"
     });
     return response.json();
-}
+} */
 
 export default async function Localization(){
 
 /*     fetch('http://localhost:3000/localization/api')
     .then((response) => console.log(response.json()))
  */
-    const data = await getData();
+    //const data = await getData();
 
     return(
         <>
             <h1>hi</h1>
-            <p>{data.country}</p>
-            <p>{data.nome}</p>
+{/*                 <p>{data.country}</p>
+                <p>{data.nome}</p> */}
         </>
     )
 

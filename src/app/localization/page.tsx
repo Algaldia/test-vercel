a8714eacfe9105a82d4c3fb82c011d4691aa9b6b
diegojsa <diegojsaraujo@gmail.com>
2024-05-15 12:44:34 -0300

    testando api

--- src/app/localization/page.tsx
+++ src/app/localization/page.tsx
@@ -1,20 +1,21 @@
 const devPath = 'http://localhost:3000/localization/api';
 const vercelPath = 'https://test-vercel-eight-indol.vercel.app/localization/api'
 
-/* async function getData() {
+async function getData() {
     "use server"
     const response = await fetch(vercelPath, {
         method: "GET"
     });
     return response.json();
-} */
+}
 
 export default async function Localization(){
 
 /*     fetch('http://localhost:3000/localization/api')
     .then((response) => console.log(response.json()))
  */
-    //const data = await getData();
+    const data = await getData();
+    console.log(data)
 
     return(
         <>

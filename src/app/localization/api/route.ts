b4268d73537c15dc2c8258a89ce82ddbbc7dfef5
diegojsa <diegojsaraujo@gmail.com>
2024-05-15 12:29:30 -0300

    trying x-vercel-ip-country

--- src/app/localization/api/route.ts
+++ src/app/localization/api/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
 
-    const country = request.headers.get("x-vercel-ip-continent")
+    const country = request.headers.get("x-vercel-ip-country")
     
     return new Response(JSON.stringify({country: country, nome: "Jomar"}), {
         headers: {
